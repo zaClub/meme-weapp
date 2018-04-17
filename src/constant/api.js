98948681785ce31c3e8929bf53f9f274e013c635
AymaxLi <848478161@qq.com>
2018-04-17 22:13:10 +0800

    feat(auth): 实现并测试登陆请求

---
+++ src/constant/api.js
@@ -0,0 +1,7 @@
+// const HOST = '119.29.199.121:3001'
+const HOST = 'http://119.29.199.121:8089'
+// const HOST = 'http://localhost:3001'
+
+export const LOGIN = `${HOST}/api/auth/login`
+
+export const UPLOAD = `${HOST}/api/pic/upload`
